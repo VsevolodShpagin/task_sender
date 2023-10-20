824f371b39433e5936e40688c411bf5d7f42da57
Vsevolod <mrehot@gmail.com>
2023-10-21 01:38:26 +0300

    html

--- src/main/java/sender/config/SpringCoreConfiguration.java
+++ src/main/java/sender/config/SpringCoreConfiguration.java
@@ -3,10 +3,12 @@ package sender.config;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan(basePackages = "sender.core")
+@PropertySource(value = "classpath:application.properties")
 public class SpringCoreConfiguration {
 
 }
--- src/main/java/sender/web_ui/controller/IndexController.java
+++ src/main/java/sender/web_ui/controller/IndexController.java
@@ -2,13 +2,19 @@ package sender.web_ui.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class IndexController {
 
     @GetMapping(value = "/")
-    public String index() {
+    public String showIndexPage() {
         return "index";
+    }
+
+    @PostMapping(value = "/")
+    public String processIndexPage() {
+        return "success";
     }
 
 }
